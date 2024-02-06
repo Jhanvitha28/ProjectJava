cea7a1052146acf4003c5334de87b2ff194c99d9
Jhanvitha28 <jhanvitha.bathula@gmail.com>
2024-02-06 15:01:14 -0600

    Expection file added

--- src/main/java/exceptionHandling/Exception.java
+++ src/main/java/exceptionHandling/Exception.java
@@ -1,2 +1,16 @@
-package exceptionHandling;public class Exception {
+package exceptionHandling;
+
+public class Exception {
+
+    public static int convertInteger(String s){
+        int i=0;
+        try{
+            i=Integer.parseInt(s);
+        }
+        catch (NumberFormatException e){
+            System.out.println("Cannot convert this string to number :"+s);
+        }
+        return i;
+    }
+
 }
--- src/test/java/exceptionHandling/ExceptionTest.java
+++ src/test/java/exceptionHandling/ExceptionTest.java
@@ -1,2 +1,33 @@
-package exceptionHandling;public class ExceptionTest {
+package exceptionHandling;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExceptionTest {
+    @Test
+    public void testWithInteger(){
+        String s="23";
+        int i = Exception.convertInteger(s);
+        assertSame(i,23);
+    }
+    @Test
+    public void testWithFloat(){
+        String s="45.67";
+        int i = Exception.convertInteger(s);
+        assertNotEquals(i,45.67);
+    }
+    @Test
+    public void testWithString(){
+        String s="test";
+        int i = Exception.convertInteger(s);
+        assertNotEquals(i,"test");
+    }
+    @Test
+    public void testWithAlphaNumeric(){
+        String s="123f";
+        int i = Exception.convertInteger(s);
+        assertNotEquals(i,"123f");
+
+    }
+
 }
